7d489a027f30feb4ea4e6aa8e39df4b6bd6cd038
SunhyuckKim <sunhyuck.kim@gmail.com>
2021-02-24 21:00:09 +0900

    HelloControllerTest.java

--- src/main/java/com/sun/springboot/web/HelloController.java
+++ src/main/java/com/sun/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.sun.springboot.web;
 
+import com.sun.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +10,11 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello찌원";
+    }
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
     }
 
 }

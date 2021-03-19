34940aaabf75334cd66da692c193d42cb8c0fa5f
SunhyuckKim <sunhyuck.kim@gmail.com>
2021-03-19 13:55:19 +0900

    3/19 ~151p까지

--- src/main/java/com/sun/springboot/service/PostsService.java
+++ src/main/java/com/sun/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.sun.springboot.service;
 
 import com.sun.springboot.domain.posts.Posts;
 import com.sun.springboot.domain.posts.PostsRepository;
+import com.sun.springboot.web.dto.PostsListResponseDto;
 import com.sun.springboot.web.dto.PostsResponseDto;
 import com.sun.springboot.web.dto.PostsSaveRequestDto;
 import com.sun.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,6 +31,14 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+        }
+
 
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));

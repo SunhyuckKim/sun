3f3bd94eaaef9f34910d99d2c772be7aaa46e253
SunhyuckKim <sunhyuck.kim@gmail.com>
2021-03-26 18:24:46 +0900

    3/26 ~200p까지

--- src/main/java/com/sun/springboot/web/IndexController.java
+++ src/main/java/com/sun/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.sun.springboot.web;
 
+import com.sun.springboot.config.auth.LoginUser;
+import com.sun.springboot.config.auth.dto.SessionUser;
 import com.sun.springboot.service.PostsService;
 import com.sun.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,9 +18,13 @@ public class IndexController {
 
     private final PostsService postsService;
 
+
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 

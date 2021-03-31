a48d65b432ff8bfe0163d1cf8b4de3f6b4eece33
SunhyuckKim <sunhyuck.kim@gmail.com>
2021-03-31 16:11:34 +0900

    3/31 ~223p까지

---
+++ src/main/java/com/sun/springboot/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.sun.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+
+public class JpaConfig {
+}
